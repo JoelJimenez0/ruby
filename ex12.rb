2e8f53555397a4686af4eeec759c306a06a5f0f0
JoelJimenez0 <joelklk10@gmail.com>
2022-07-15 17:05:45 -0400

    added a comment

--- ex12.rb
+++ ex12.rb
@@ -7,6 +7,6 @@ puts "A bigger number is #{bigger}."
 print "Give me another number: "
 another = gets.chomp
 number = another.to_f
-
+#use .to_i to add numbers without decimals - use .to_f to add decimal numbers.
 smaller = number / 100
 puts "A smaller number is #{smaller}."
