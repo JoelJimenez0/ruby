43ecec1d87714f3311468fe718475edf776f0e12
JoelJimenez0 <joelklk10@gmail.com>
2022-07-15 17:03:58 -0400

    inputs

---
+++ ex11.rb
@@ -0,0 +1,10 @@
+print "What is your name?"
+name = gets.chomp
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, your name is #{name}, you're #{age} old, #{height} tall and #{weight} heavy."
